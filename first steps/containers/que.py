39d66b9376ecc11ad5a080f716b9d5639a21f2f8
Diezel228 <71187167+Diezel228@users.noreply.github.com>
2023-12-19 09:02:38 -0600

    oh yea!!!!

--- first steps/containers/que.py
+++ first steps/containers/que.py
@@ -2,30 +2,30 @@
 
 school = [
     [    
-        ["sara", "kim", "anne", "eva"]
-        ["johan", "collin", "sam", "alex"]
-        ["luke", "sara", "haley", "jennifer"]
+        ["sara", "kim", "anne", "eva"],#make sure to add the commas to create an array and not tuple!
+        ["johan", "collin", "sam", "alex"],
+        ["luke", "sara", "haley", "jennifer"],
         ["katy", "mara", "max", "roy"]
     ],
     [
-        ["anne", "leo", "sasha", "tim"]
-        ["claire", "guy", "eva", "zoe"]
-        ["lisa", "max", "evan", "chloe"]
+        ["anne", "leo", "sasha", "tim"],
+        ["claire", "guy", "eva", "zoe"],
+        ["lisa", "max", "evan", "chloe"],
         ["brent", "sam", "sarah", "anne"]
     ],
     [
-        ["maria", "julian", "chris", "tom"]
-        ["zoe", "anne", "kim", "leo"]
-        ["vera", "pim", "leo", "guy"]
+        ["maria", "julian", "chris", "tom"],
+        ["zoe", "anne", "kim", "leo"],
+        ["vera", "pim", "leo", "guy"],
         ["anne", "sofie", "max", "joe"]
     ],
     [
-        ["sam", "max", "joe", "anne"]
-        ["sofie", "lisa", "tim", "sasha"]
-        ["claire", "sara", "leo", "kim"]
+        ["sam", "max", "joe", "anne"],
+        ["sofie", "lisa", "tim", "sasha"],
+        ["claire", "sara", "leo", "kim"],
         ["zoe", "guy", "anna", "eva"]
     ]
 ]
 
-best_Student = school [3,1,2]
+best_Student = school [3][1][2]
 print(best_Student)#should be tim..
