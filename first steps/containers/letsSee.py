0157726b1065f2ee64575b0129d22c20807125b0
Diezel228 <71187167+Diezel228@users.noreply.github.com>
2023-12-19 07:42:52 -0600

    run in gh

--- first steps/containers/letsSee.py
+++ first steps/containers/letsSee.py
@@ -1,2 +1,6 @@
-name = "Lydia"# variables get saved straight to memomry directly with string
-list = [24,34,21,76] #
+name = "Lydia"# variables get saved straight to memory directly with string
+list = [24,34,21,76] #creates a var thats a referance to a memory address
+list2 = list #creates a copy of the address in memory first, change the first and second changes too.
+#in order to have a for this not to happen(splice) the list
+list3 = list2[0:3]
+print(name, "\n" ,str(list) ,"\n" , str(list2), "\n", str(list3))
