6276be88eb75bc22a45d53741436096b7c5b80ba
Diezel228 <71187167+Diezel228@users.noreply.github.com>
2023-12-19 08:07:33 -0600

    .

--- first steps/containers/letsSee.py
+++ first steps/containers/letsSee.py
@@ -4,3 +4,14 @@ list2 = list #creates a copy of the address in memory first, change the first an
 #in order to have a for this not to happen(splice) the list
 list3 = list2[0:3]
 print(name, "\n" ,str(list) ,"\n" , str(list2), "\n", str(list3))
+
+print(list[1:])#slice from this index to the end
+print(list[:4])#slice up to index (not specifying eg. [:] uses all items)
+#you can specify negatives too!!!!
+#    ////////print(list[:]) creates copy in memory
+del list3 [:2]#modifies og list
+
+
+#////////////////////////finding 
+print(34 in list)
+print(99 not in list)
