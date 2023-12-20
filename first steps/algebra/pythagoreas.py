92e3d38946ec8ec9e90a97aed17945750557113a
Diezel228 <71187167+Diezel228@users.noreply.github.com>
2023-12-20 04:56:17 +0000

    .

--- first steps/algebra/pythagoreas.py
+++ first steps/algebra/pythagoreas.py
@@ -1,5 +1,16 @@
-def pythagoreas (): 
-    return a = int(print("Enter height:"))
-    return b = int(print("Enter width:"))
-    c = a+b
-    return print(c)
+def pythagoreas(a, b, c=0): 
+    import math
+    c = a*a + b*b
+    c = math.sqrt(c)
+    
+    arcsinA = a/c
+    arcsinB = b/c
+    area = a*b/2
+    perimeter = a+b+c
+    h = a*b/c
+    list01=[c,arcsinA,arcsinB,area,perimeter,h sep=", "]
+    return list01
+
+triangle = pythagoreas(200,36)
+
+print(triangle)
